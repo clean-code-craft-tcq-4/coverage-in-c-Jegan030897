0d9ec053a985fc1dbb8d360b7d1fc6773d7fe1a4
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-24 10:21:49 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,6 +1,8 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
+BreachType breachType;
+
 int Battery_TempLimit[NUMBER_OF_BATTERY_TYPE][6] = 
 {PASSIVE_COOLING_LOW_LIMIT,  PASSIVE_COOLING_HIGH_LIMIT,  MED_ACTIVE_COOLING_LOW_LIMIT,  MED_ACTIVE_COOLING_HIGH_LIMIT,  HI_ACTIVE_COOLING_LOW_LIMIT,  HI_ACTIVE_COOLING_HIGH_LIMIT};
 
@@ -16,44 +18,44 @@ BreachType classifyTemperatureBreach(BatteryCharacter batteryChar, double temper
 }
 
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, swType getSWtype) {
-  BreachType breachType = classifyTemperatureBreach(batteryChar, temperatureInC);
+  breachType = classifyTemperatureBreach(batteryChar, temperatureInC);
   
   void (*selectSW[NUMBER_OF_SW])(BreachType, AlertTarget) = {swforProduction, swforTesting};
   selectSW[getSWtype](breachType, alertTarget);
 }
 
-void swforProduction(BreachType breachType, AlertTarget alertTarget)
+void swforProduction(BreachType getbreachType, AlertTarget alertTarget)
 {
   if(TO_CONTROLLER == alertTarget){
-    sendToController(breachType);
+    sendToController(getbreachType);
   }else {
-    sendToEmail(breachType);
+    sendToEmail(getbreachType);
   }
 }
 
-void swforTesting(BreachType breachType, AlertTarget alertTarget)
+void swforTesting(BreachType getbreachType, AlertTarget alertTarget)
 {
   if(TO_CONTROLLER == alertTarget){
-    sendToController_forTesting(breachType);
+    sendToController_forTesting(getbreachType);
   }else {
-    sendToEmail_forTesting(breachType);
+    sendToEmail_forTesting(getbreachType);
   }
 }
 
-void sendToController_forTesting(BreachType breachType) {
-  SEND_MSG_TO_CONTROLLER(breachType);
+void sendToController_forTesting(BreachType getbreachType) {
+  SEND_MSG_TO_CONTROLLER(getbreachType);
 }
 
-void sendToEmail_forTesting(BreachType breachType) {
-  SEND_MSG_THROUGH_EMAIL(breachType); 
+void sendToEmail_forTesting(BreachType getbreachType) {
+  SEND_MSG_THROUGH_EMAIL(getbreachType); 
 }
 
-void sendToController(BreachType breachType) {
+void sendToController(BreachType getbreachType) {
   //will be processed in real time hw setup
-  SEND_MSG_TO_CONTROLLER(breachType);
+  SEND_MSG_TO_CONTROLLER(getbreachType);
 }
 
-void sendToEmail(BreachType breachType) {
+void sendToEmail(BreachType getbreachType) {
   //will be processed in real time
-  SEND_MSG_THROUGH_EMAIL(breachType);
+  SEND_MSG_THROUGH_EMAIL(getbreachType);
 }
