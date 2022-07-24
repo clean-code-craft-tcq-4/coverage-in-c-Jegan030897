d3470f747cc157876ce6db01b2b78c7fef456d75
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-24 10:28:58 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,31 +8,14 @@ TEST_CASE("infers the breach according to limits") {
   batteryChar.coolingType = PASSIVE_COOLING;
   batteryChar.brand = LI_ION_BATTERY;
   
-  checkAndAlert(TO_CONTROLLER,batteryChar,-5,SW_FOR_TESTING);
+  checkAndAlert(TO_CONTROLLER, batteryChar, -5, SW_FOR_TESTING);
   REQUIRE(breachType == TOO_LOW);
-  /*
-  REQUIRE(inferBreach(20, 30, 35) == TOO_LOW);
-  REQUIRE(inferBreach(36, 30, 35) == TOO_HIGH);
-  REQUIRE(inferBreach(35, 30, 35) == NORMAL);
-  REQUIRE(inferBreach(30, 30, 35) == NORMAL);*/
-}
-/*
-TEST_CASE("classify breach") {
-  BatteryCharacter batteryChar;
-  batteryChar.coolingType = PASSIVE_COOLING;
-  batteryChar.brand = LI_ION_BATTERY;
-  REQUIRE(classifyTemperatureBreach(batteryChar, -5) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 36) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 35) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 30) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 32) == NORMAL);
+  
   batteryChar.coolingType = MED_ACTIVE_COOLING;
-  REQUIRE(classifyTemperatureBreach(batteryChar, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 41) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 36) == NORMAL);
-  batteryChar.coolingType = HI_ACTIVE_COOLING ;
-  REQUIRE(classifyTemperatureBreach(batteryChar, 35) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 55) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(batteryChar, 45) == NORMAL);
+  checkAndAlert(TO_EMAIL, batteryChar, 40, SW_FOR_TESTING);
+  REQUIRE(breachType == NORMAL);
+  
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_EMAIL, batteryChar, 46, SW_FOR_TESTING);
+  REQUIRE(breachType == TOO_HIGH);
 }
-*/
